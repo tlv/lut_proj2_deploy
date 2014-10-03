0d4fcd225037e79c345f8b553444c18d1cd7b6ba
lut <lut94mit@gmail.com>
2014-10-03 01:01:05 -0400

    asdfg

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ var params = require('express-params');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/fritter');
+
+var connection_string = 'localhost/fritter';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/mymongo';
+}
+
+var db = monk(connection_string);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
